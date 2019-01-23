e11dbe48614288aafbc99fae0fdf5f048d5bf986
Ross Markello <rossmarkello@gmail.com>
2019-01-23 15:31:13 -0500

    [FIX] Wrong imports in __init__.py

--- netneurotools/__init__.py
+++ netneurotools/__init__.py
@@ -1,9 +1,9 @@
-__all__ = ['__version__', 'algorithms', 'metrics',
+__all__ = ['__version__', 'metrics', 'stats', 'utils',
            'consensus_modularity', 'func_consensus', 'struct_consensus',
            'plot_mod_heatmap', 'plot_conte69', 'plot_fsaverage']
 
 from .info import (__version__)
-from . import algorithms, metrics
+from . import metrics, stats, utils
 from .modularity import consensus_modularity
 from .networks import func_consensus, struct_consensus
 from .plotting import plot_mod_heatmap, plot_conte69, plot_fsaverage
