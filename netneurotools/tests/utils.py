b9bc5681ab4314758ff71eb56ad0193954122df1
Ross Markello <rossmarkello@gmail.com>
2019-01-25 15:13:28 -0500

    [ENH] Better correlations with make_correlated_xy
    Includes a tolerance parameter to ensure that correlation between
    generated vectors is close to specification.

--- netneurotools/tests/utils.py
+++ netneurotools/tests/utils.py
@@ -7,9 +7,9 @@ import numpy as np
 from sklearn.utils.validation import check_random_state
 
 
-def make_correlated_xy(corr=0.85, size=10000, seed=None):
+def make_correlated_xy(corr=0.85, size=10000, seed=None, tol=0.05):
     """
-    Generates two random vectors that are correlated to approximately `corr`
+    Generates random vectors that are correlated to approximately `corr`
 
     Parameters
     ----------
@@ -23,11 +23,14 @@ def make_correlated_xy(corr=0.85, size=10000, seed=None):
         Desired size of the generated vectors. Default: 1000
     seed : {int, np.random.RandomState instance, None}, optional
         Seed for random number generation. Default: None
+    tol : [0, 1] float, optional
+        Tolerance of correlation between generated `vectors` and specified
+        `corr`. Default: 0.05
 
     Returns
     -------
     vectors : numpy.ndarray
-        Random vectors of size ``size`` with correlation specified by `corr`
+        Random vectors of size `size` with correlation specified by `corr`
     """
 
     rs = check_random_state(seed)
@@ -39,10 +42,17 @@ def make_correlated_xy(corr=0.85, size=10000, seed=None):
     # if we're given a correlation matrix, assume `N` vectors are desired
     elif isinstance(corr, (list, np.ndarray)):
         corr = np.asarray(corr)
+        if np.any(np.diag(corr) != 1):
+            raise ValueError('Diagonal of `corr` must be set to 1.')
         covs = corr * 0.111
     means = [0] * len(covs)
 
     # generate the variables
-    vectors = rs.multivariate_normal(mean=means, cov=covs, size=size).T
+    count = 0
+    while count < 500:
+        vectors = rs.multivariate_normal(mean=means, cov=covs, size=size).T
+        if np.any(np.abs(np.corrcoef(vectors) - (covs / 0.111)) > tol):
+            break
+        count += 1
 
     return vectors
