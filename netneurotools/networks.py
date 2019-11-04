9493788edb8eeb87dab956627a2d88e9698773b5
Golia Shafiei <31447439+gshafiei@users.noreply.github.com>
2019-11-04 10:20:06 -0500

    Update netneurotools/networks.py
    Co-Authored-By: Ross Markello <rossmarkello@gmail.com>

--- netneurotools/networks.py
+++ netneurotools/networks.py
@@ -59,7 +59,7 @@ def func_consensus(data, n_boot=1000, ci=95, seed=None):
 
     # group-average functional connectivity matrix desired instead of bootstrap
     if n_boot == 0 or n_boot is None:
-        if type(data) is list:
+        if isinstance(data, list):
             corrs = [np.corrcoef(data[sub]) for sub in range(len(data))]
         else:
             corrs = [np.corrcoef(data[..., sub]) for sub in
