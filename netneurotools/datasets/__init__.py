21132b4a43f992c4504fd4edad3c29bb81dfeed3
Ross Markello <rossmarkello@gmail.com>
2019-11-04 09:41:23 -0500

    [REF] Need to make fetch_schaefer2018 accessible
    Want to be able to use nntools.datasets.fetch_schaefer2018(), so it
    needs to be present in the __init__.py

--- netneurotools/datasets/__init__.py
+++ netneurotools/datasets/__init__.py
@@ -5,11 +5,11 @@ Functions for fetching and generating datasets
 __all__ = [
     'fetch_cammoun2012', 'fetch_pauli2018', 'fetch_fsaverage', 'fetch_conte69',
     'fetch_connectome', 'available_connectomes', 'fetch_vazquez_rodriguez2019',
-    'fetch_mirchi2018', 'make_correlated_xy'
+    'fetch_mirchi2018', 'make_correlated_xy', 'fetch_schaefer2018'
 ]
 
 from .fetchers import (fetch_cammoun2012, fetch_pauli2018, fetch_fsaverage,
                        fetch_conte69, fetch_connectome, available_connectomes,
-                       fetch_vazquez_rodriguez2019)
+                       fetch_vazquez_rodriguez2019, fetch_schaefer2018)
 from .generators import (make_correlated_xy)
 from .mirchi import (fetch_mirchi2018)
