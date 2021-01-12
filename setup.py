caca281ec4d1dd2ab270b1430dd0be5c18ae2789
Ross Markello <rossmarkello@gmail.com>
2021-01-12 15:29:49 -0500

    [MNT] More build overhauling

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python
+import os
 from setuptools import setup
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 import versioneer
 
 if __name__ == "__main__":
