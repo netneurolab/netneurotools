6d3d4285c0fddb9094be784bf37c1978d001663e
Ross Markello <rossmarkello@gmail.com>
2021-01-28 11:30:56 -0500

    [TEST] Xvfb for pysurfer?

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -2,12 +2,16 @@
 
 echo "Installing dependencies"
 
-python -m pip install --upgrade pip build
+python -m pip install --upgrade pip build wheel
 python -m pip install --upgrade -r requirements.txt
 
 if [ -n "${OPTIONAL_DEPENDS}" ]; then
     for DEP in ${OPTIONAL_DEPENDS}; do
-        pip install $DEP || true
+        if [ ${DEP} == "mayavi" ]; then
+            python -m pip install numpy vtk
+            sudo apt-get install -y xvfb x11-utils libxkbcommon-x11-0
+        fi
+        python -m pip install $DEP || true
     done
 fi
 
