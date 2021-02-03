a903253743f0c1e835d92c4d9f6a5466e8f5b8a3
Ross Markello <rossmarkello@gmail.com>
2021-02-03 14:52:23 -0500

    [TEST] Fail fast fail early

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -9,10 +9,22 @@ if [ -n "${OPTIONAL_DEPENDS}" ]; then
     for DEP in ${OPTIONAL_DEPENDS}; do
         if [ ${DEP} == "mayavi" ]; then
             python -m pip install numpy vtk
-            sudo apt-get install -y xvfb x11-utils libxkbcommon-x11-0
+            sudo apt-get install -y xvfb \
+                                    x11-utils \
+                                    mencoder \
+                                    libosmesa6 \
+                                    libglx-mesa0 \
+                                    libopengl0 \
+                                    libglx0 \
+                                    libdbus-1-3 \
+                                    qt5-default
         fi
         python -m pip install $DEP || true
     done
 fi
 
 echo "Finished installed dependencies"
+
+if [[ ${OPTIONAL_DEPENDS} == *"mayavi"* ]]; then
+    LIBGL_DEBUG=verbose /usr/bin/xvfb-run --auto-servernum python -c "from mayavi import mlab; import matplotlib.pyplot as plt; mlab.figure(); plt.figure()"
+fi
