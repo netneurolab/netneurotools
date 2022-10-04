9fa5f5c73490a0b18d9b1aa9f1710b47bee73cfd
Zhen-Qi Liu <liuzhenqi77@gmail.com>
2022-10-04 11:20:42 -0400

    [MNT] Update versioneer

--- netneurotools/__init__.py
+++ netneurotools/__init__.py
@@ -2,6 +2,5 @@ __all__ = [
     '__version__',
 ]
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
