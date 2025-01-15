a4060c93814b91c61e422604b7cc660cd2f90cfe
Zhen-Qi Liu <liuzhenqi77@gmail.com>
2025-01-15 15:10:03 -0500

    [FIX] update scipy documentation link to the correct reference

--- docs/conf.py
+++ docs/conf.py
@@ -112,7 +112,7 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'matplotlib': ('https://matplotlib.org/stable', None),
     'numpy': ('https://numpy.org/doc/stable/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy', None),
     'sklearn': ('https://scikit-learn.org/stable', None),
     'pandas': ('https://pandas.pydata.org/pandas-docs/stable', None),
     'pyvista': ('https://docs.pyvista.org/', None),
